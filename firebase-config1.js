d027767ef1a02e1f9c42f28e5c2367a75395eebb
bilsonhere <121447291+bilsonhere@users.noreply.github.com>
2025-08-05 15:59:51 +0545

    Create firebase-config1.js

---
+++ firebase-config1.js
@@ -0,0 +1,23 @@
+export const firebaseConfig = {
+
+
+  apiKey: "AIzaSyB9D5u5hukU-6vdPZDXaE6msj1Hp1ZR3mk",
+
+  authDomain: "seriousasl.firebaseapp.com",
+
+  projectId: "seriousasl",
+
+  storageBucket: "seriousasl.firebasestorage.app",
+
+  messagingSenderId: "835405980832",
+
+  appId: "1:835405980832:web:60a7062645587f869a15c5",
+
+  measurementId: "G-KHRTWVE0FH"
+
+
+
+
+
+
+};
